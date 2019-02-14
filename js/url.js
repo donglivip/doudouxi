2a28719d71357940d03a8e46275aa8629b260827
dongxiaoli <1076454380@qq.com>
2019-02-14 17:34:47 +0800

    bug

--- js/url.js
+++ js/url.js
@@ -1,3 +1,5 @@
-//					localStorage.setItem('myurl', 'http://27.54.248.14:8080')
-//					localStorage.setItem('myurl','http://192.168.2.116')
+//					localStorage.setItem('myurl', 'https://doudouxi.u581.com')
+////					localStorage.setItem('myurl','http://192.168.2.116')
 					localStorage.setItem('myurl','https://console.pingfanren.com.cn')
+					
+var url='https://console.pingfanren.com.cn'
